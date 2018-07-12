8d0a2aba523e12775562edac65a4f7031eae62c6
Drew Willey <andwilley@gmail.com>
2018-07-11 20:34:54 -0700

    Update App.css: add color to icons. Update FlightBox: add break between goes.

--- src/components/FlightBox.tsx
+++ src/components/FlightBox.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Flight from './Flight';
 import { IErrors, IFlights } from '../types/State';
 import IconButton from './IconButton';
+import * as Moment from 'moment';
 
 interface IFlightBoxProps {
     dayId: string;
@@ -12,6 +13,12 @@ interface IFlightBoxProps {
     onDelFlightClick: (flight: IFlights, dayId: string) => (e: any) => void;
 }
 
+interface IFlightListAcc {
+    goStartTime: Moment.Moment;
+    goEndTime: Moment.Moment;
+    flightComponents: JSX.Element[];
+}
+
 const FlightBox: React.SFC<IFlightBoxProps> = ({
     dayId,
     flights,
@@ -20,16 +27,42 @@ const FlightBox: React.SFC<IFlightBoxProps> = ({
     onAddFlightClick,
     onDelFlightClick,
 }) => {
-    const flightComponents = flights.map(flight =>
-        (
-        <Flight
-            key={flight.id}
-            flight={flight}
-            dayId={dayId}
-            onDelFlightClick={onDelFlightClick}
-            errors={errors[flight.id] ? errors[flight.id] : []}
-        />
-        ));
+    const flightComponents = flights.reduce((acc: IFlightListAcc, flight, i) => {
+        if (i === 0) {
+            acc.goStartTime =
+                Moment(`${dayId} ${flight.times.takeoff.slice(0, 2)}:${flight.times.takeoff.slice(2, 4)}:00.000`,
+            'YYYY-MM-DD HH:mm:ss.SSS');
+            acc.goEndTime =
+                Moment(`${dayId} ${flight.times.land.slice(0, 2)}:${flight.times.land.slice(2, 4)}:00.000`,
+            'YYYY-MM-DD HH:mm:ss.SSS').add(59, 'minutes');
+        }
+        const flightTakeoffTime =
+            Moment(`${dayId} ${flight.times.takeoff.slice(0, 2)}:${flight.times.takeoff.slice(2, 4)}:00.000`,
+                'YYYY-MM-DD HH:mm:ss.SSS');
+        const flightLandTime =
+            Moment(`${dayId} ${flight.times.land.slice(0, 2)}:${flight.times.land.slice(2, 4)}:00.000`,
+                    'YYYY-MM-DD HH:mm:ss.SSS');
+        const flightComponent = (
+            <Flight
+                key={flight.id}
+                flight={flight}
+                dayId={dayId}
+                onDelFlightClick={onDelFlightClick}
+                errors={errors[flight.id] ? errors[flight.id] : []}
+            />
+        );
+        if (flightTakeoffTime.isAfter(acc.goEndTime)) {
+            acc.flightComponents.push(
+                <div className="w-100" />
+            );
+            acc.flightComponents.push(flightComponent);
+            acc.goStartTime = flightTakeoffTime;
+            acc.goEndTime = flightLandTime;
+        } else {
+            acc.flightComponents.push(flightComponent);
+        }
+        return acc;
+    }, {goStartTime: Moment(), goEndTime: Moment(), flightComponents: []}).flightComponents;
     const simComponents = sims.map(flight =>
         (
         <Flight
