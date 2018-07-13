2d284daeb94e760bf9e6a175c6ddc9c59f4335ee
Drew Willey <andwilley@gmail.com>
2018-07-13 08:13:54 -0700

    added sortie and time to header of flights.

--- src/components/Flight.tsx
+++ src/components/Flight.tsx
@@ -6,6 +6,8 @@ import SortieBoxContainer from '../containers/SortieBoxContainer';
 import NoteBoxContainer from '../containers/NoteBoxContainer';
 import ErrorList from './ErrorList';
 import IconButton from './IconButton';
+import { conv24HrTimeToMoment } from '../types/utilFunctions';
+import * as Moment from 'moment';
 
 interface IFlightProps {
     flight: IFlights;
@@ -14,10 +16,42 @@ interface IFlightProps {
     onDelFlightClick: (flight: IFlights, dayId: string) => (e: any) => void;
 }
 
+const getFlightTime = (flight: IFlights, dayId: string): string => {
+    const takeoff = conv24HrTimeToMoment(flight.times.takeoff, dayId);
+    const land = conv24HrTimeToMoment(flight.times.land, dayId);
+    if (!takeoff.isValid() || !land.isValid()) {
+        return '0.0';
+    }
+    return Moment.duration(land.diff(takeoff)).asHours().toFixed(1);
+};
+
 const Flight: React.SFC<IFlightProps> = ({ flight, errors, onDelFlightClick, dayId }) => (
     <div className="col-xl-3 col-lg-4 col-md-6">
         <div className="card mb-3 box-shadow">
             <div className="card-header bg-dark">
+                <IconButton
+                    icon="plane"
+                    viewBox="0 0 21 22"
+                    svgClass="crew-icon"
+                    pointer={false}
+                    size={12}
+                    style={{
+                        margin: '0 .2rem 0 0',
+                    }}
+                >
+                <span className="text-light">{flight.sorties.length}</span>
+                </IconButton>
+                <IconButton
+                    icon="clock"
+                    svgClass="crew-icon"
+                    pointer={false}
+                    size={12}
+                    style={{
+                        margin: '0 .2rem 0 .5rem',
+                    }}
+                >
+                <span className="text-light">{getFlightTime(flight, dayId)}</span>
+                </IconButton>
                 <IconButton
                     onClick={onDelFlightClick(flight, dayId)}
                     icon="trash"
