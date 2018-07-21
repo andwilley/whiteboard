5d39af5a6b20dd9b4eaf09676b2b5118e56fc426
Drew Willey <andwilley@gmail.com>
2018-07-21 06:18:21 -0700

    Started refactor to class based PureComponent.

--- src/components/CrewList.tsx
+++ src/components/CrewList.tsx
@@ -21,65 +21,70 @@ interface ICrewListProps {
     onSnivEditClick: (sniv: IAddUpdateSnivArgs) => any;
 }
 
-const CrewList: React.SFC<ICrewListProps> = ({
-    aircrewList,
-    crewDayAndWorkDay,
-    unavailableAircrewIds,
-    daySnivs,
-    showSnivs,
-    dayId,
-    onAircrewClick,
-    onAircrewXClick,
-    onAircrewEditClick,
-    onSnivXClick,
-    onSnivEditClick,
-    }) => {
-    const pilotList: JSX.Element[] = [];
-    const wsoList: JSX.Element[] = [];
-    aircrewList.forEach((aircrew: IAircrewWithPucks) => {
-        const aircrewComponent = (
-            <Aircrew
-                key={aircrew.id}
-                aircrew={aircrew}
-                crewDayAndWorkDay={crewDayAndWorkDay[aircrew.id]}
-                unavailable={unavailableAircrewIds.indexOf(aircrew.id) > -1 ? true : false}
-                snivs={daySnivs.filter(sniv => sniv.aircrewIds.indexOf(aircrew.id) > -1)}
-                showSnivs={showSnivs}
-                dayId={dayId}
-                onAircrewClick={() => onAircrewClick(aircrew)}
-                onAircrewXClick={() => onAircrewXClick(aircrew.id)}
-                onAircrewEditClick={() => onAircrewEditClick(aircrew)}
-                onSnivXClick={onSnivXClick}
-                onSnivEditClick={onSnivEditClick}
-            />
+class CrewList extends React.PureComponent<ICrewListProps> {
+    render() {
+        const {
+            aircrewList,
+            crewDayAndWorkDay,
+            unavailableAircrewIds,
+            daySnivs,
+            showSnivs,
+            dayId,
+            onAircrewClick,
+            onAircrewXClick,
+            onAircrewEditClick,
+            onSnivXClick,
+            onSnivEditClick,
+        } = this.props;
+        const pilotList: JSX.Element[] = [];
+        const wsoList: JSX.Element[] = [];
+        aircrewList.forEach((aircrew: IAircrewWithPucks) => {
+            const aircrewComponent = (
+                <Aircrew
+                    key={aircrew.id}
+                    aircrew={aircrew}
+                    crewDayAndWorkDay={crewDayAndWorkDay[aircrew.id]}
+                    unavailable={unavailableAircrewIds.indexOf(aircrew.id) > -1 ? true : false}
+                    snivs={daySnivs.filter(sniv => sniv.aircrewIds.indexOf(aircrew.id) > -1)}
+                    showSnivs={showSnivs}
+                    dayId={dayId}
+                    onAircrewClick={() => onAircrewClick(aircrew)}
+                    onAircrewXClick={() => onAircrewXClick(aircrew.id)}
+                    onAircrewEditClick={() => onAircrewEditClick(aircrew)}
+                    onSnivXClick={onSnivXClick}
+                    onSnivEditClick={onSnivEditClick}
+                />
+            );
+            aircrew.seat === 'pilot' ? pilotList.push(aircrewComponent) : wsoList.push(aircrewComponent);
+        });
+        return (
+            <div>
+                <hr />
+                <h6
+                    className={'sidebar-heading d-flex justify-content-between \
+align-items-center px-3 mt-4 mb-1 text-muted'}
+                >
+                    Pilots
+                </h6>
+                <ul className="nav flex-column">
+                    {pilotList}
+                </ul>
+                <hr />
+                <h6
+                    className={'sidebar-heading d-flex justify-content-between \
+align-items-center px-3 mt-4 mb-1 text-muted'}
+                >
+                    WSOs
+                </h6>
+                <ul className="nav flex-column">
+                    {wsoList}
+                </ul>
+                <hr />
+                <AddUpdateAircrewFormContainer />
+                <AddSnivFormContainer />
+            </div>
         );
-        aircrew.seat === 'pilot' ? pilotList.push(aircrewComponent) : wsoList.push(aircrewComponent);
-    });
-    return (
-        <div>
-            <hr />
-            <h6
-                className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
-            >
-                Pilots
-            </h6>
-            <ul className="nav flex-column">
-                {pilotList}
-            </ul>
-            <hr />
-            <h6
-                className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
-            >
-                WSOs
-            </h6>
-            <ul className="nav flex-column">
-                {wsoList}
-            </ul>
-            <hr />
-            <AddUpdateAircrewFormContainer />
-            <AddSnivFormContainer />
-        </div>
-    );
-};
+    }
+}
 
 export default CrewList;
--- src/components/CrewListBox.tsx
+++ src/components/CrewListBox.tsx
@@ -2,13 +2,17 @@ import * as React from 'react';
 import CrewSearchBoxContainer from '../containers/CrewSearchBoxContainer';
 import VisibleCrewList from '../containers/VisibleCrewList';
 
-const CrewListBox: React.SFC = () => (
-    <nav className="col-md-2 d-none d-md-block bg-dark sidebar">
-        <div className="sidebar-sticky">
-            <CrewSearchBoxContainer />
-            <VisibleCrewList />
-        </div>
-    </nav>
-);
+class CrewListBox extends React.PureComponent {
+    render() {
+        return (
+            <nav className="col-md-2 d-none d-md-block bg-dark sidebar">
+                <div className="sidebar-sticky">
+                    <CrewSearchBoxContainer />
+                    <VisibleCrewList />
+                </div>
+            </nav>
+        );
+    }
+}
 
 export default CrewListBox;
